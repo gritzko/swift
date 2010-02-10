3ef94ce76774a64858de5a32d78bba0f50e34c05
Victor Grishchenko (mughal) <victor.grishchenko@gmail.com>
2010-02-10 18:47:56 +0100

    netem settings

--- mfold/net.lossy.sh
+++ mfold/net.lossy.sh
@@ -1,3 +1,3 @@
 touch .netem-on
 
-sudo tc qdisc add dev eth0 root netem delay 100ms loss 4.0%
+sudo tc qdisc add dev eth0 root netem delay 100ms loss 5.0%
--- mfold/net.messy.sh
+++ mfold/net.messy.sh
@@ -1,3 +1,3 @@
 touch .netem-on
 
-sudo tc qdisc add dev eth0 root netem delay 100ms 40ms 25%
+sudo tc qdisc add dev eth0 root netem delay 100ms 20ms 25%
